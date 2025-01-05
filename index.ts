2f7ae65cdc01dd82797b48ab4a77e1ac9664d17d
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-01-06 06:24:10 +0900

    test

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { sum } from "./libs/common-utils/sum";
+import { consoleLog, sum } from "./libs/common-utils/sum";
 
 const result = sum(2, 3, 4);
 console.log("sum result:", result);
+console.log("consoleLog", consoleLog(1, 2, 3));
